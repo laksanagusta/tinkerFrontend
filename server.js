4acda09bcd687babb7e6f4073587367d185e03d4
dika <dikalaksana>
2020-12-21 05:55:36 +0800

    Any port

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require("path")
 
 app.use(express.static(path.join(__dirname, "client", "build")))
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
-})
+const port = process.env.port || 5000;
+
+app.listen(port, () => {
+    console.log('itsworking')
+});
