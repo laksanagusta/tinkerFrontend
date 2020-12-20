066f66c9ed29639d5b63bc4523bd1eac97305edc
dika <dikalaksana>
2020-12-21 05:46:56 +0800

    change port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const PORT = 8000
+const PORT = 5000
 const path = require("path")
 
 app.use(express.static(path.join(__dirname, "client", "build")))
